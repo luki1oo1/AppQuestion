8729d3b8c63556f3d8adf2bbfcc7bf72c3f1f348
luki1oo1 <34570907+luki1oo1@users.noreply.github.com>
2021-11-24 22:46:45 +0100

    push app

---
+++ src/app/app.component.ts
@@ -0,0 +1,53 @@
+import {Component, OnInit} from '@angular/core';
+interface taskQuestion {
+  question: string,
+  a: string,
+  b: string,
+  c: string,
+  d: string,
+  correct: string
+}
+@Component({
+  selector: 'app-root',
+  templateUrl: `./app.component.html`,
+  styleUrls: ['./app.component.css']
+})
+
+
+export class AppComponent {
+  title = 'app-quiz';
+  taskQuestions :  taskQuestion[] = [
+    {
+      question: "Which language runs in a web browser?",
+      a: "Java",
+      b: "C",
+      c: "Python",
+      d: "JavaScript",
+      correct: "d"
+    },
+    {
+      question: "What does CSS stand for?",
+      a: "Central Style Sheets",
+      b: "Cascading Style Sheets",
+      c: "Cascading Simple Sheets",
+      d: "Cars SUVs Sailboats",
+      correct: "b"
+    },
+    {
+      question: "What does HTML stand for?",
+      a: "Hypertext Markup Language",
+      b: "Hypertext Markdown Language",
+      c: "Hyperloop Machine Language",
+      d: "Helicopters Terminals Motorboats Lamborginis",
+      correct: "a"
+    },
+    {
+      question: "What year was JavaScript launched?",
+      a: "1996",
+      b: "1995",
+      c: "1994",
+      d: "none of the above",
+      correct: "b"
+    }
+  ];
+}
